c42e7c7167bb15bebd1857e328273152fe51d6c5
vikolga <ov.reatch@gmail.com>
2024-09-22 12:26:37 +0300

    Добавить README.md Убрать лишние комментарии

---
+++ README.md
@@ -0,0 +1,61 @@
+# тестовое задание - Парсер решения с сайта https://mathsolver.microsoft.com/ru/solve-problem/x%5E%7B2%7D-3x%2B2%3D0
+
+Проект парсинга решений математических задач
+Выполняется парсинг данных со страницы https://mathsolver.microsoft.com/ru/solve-problem/x%5E%7B2%7D-3x%2B2%3D0.Парсер подготавливает данные и сохраняет их в формат json.
+
+[![Python](https://img.shields.io/badge/-Python-464646?style=flat&logo=Python&logoColor=ffffff&color=013220)](https://www.python.org/)&nbsp;
+[![BeautifulSoup](https://img.shields.io/badge/BeautifulSoup-DC382D.svg?&style=flat&logo=BeautifulSoup&logoColor=white)]&nbsp;
+[![Selenium]<img src=https://img.shields.io/badge/Framework-Selenium-brightgreen>]&nbsp;
+[![Webdriver]<img src=https://img.shields.io/badge/WebDriver-ChromeDriver-blue>]&nbsp;
+
+
+## Оглавление
+1. [Описание](#описание)
+2. [Стек технологий](##стек-технологий)
+3. [Как запустить проект](##как-запустить-проект)
+4. [Автор проекта](##автор-проекта)
+
+
+## Описание проекта:
+
+Парсер, собирающий решение с сайта https://mathsolver.microsoft.com/ru/solve-problem/x%5E%7B2%7D-3x%2B2%3D0
+Вся собранная информация сохраняется в файлы с расширением **json**:
+
+## Стек технологий
+- Python, BS4, Selenium, WebDriver
+
+## Как запустить проект
+
+Клонировать проект из репозитория
+```
+git@github.com:vikolga/math_test.git
+```
+
+Создать, активировать виртуальное окружение и в него установить зависимости:
+
+```
+python -m venv .env
+```
+
+```
+source .env/Scripts/activate
+```
+
+```
+pip install -r requirements.txt
+```
+
+Запустить парсер из командной строки:
+
+```
+python main.py
+```
+
+Результатом работы парсера будет создание файла:
+
+```
+answers.json
+```
+
+## Автор проекта
+_[Ольга Викторова](https://github.com/vikolga/)_, python-developer
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from time import sleep
 import requests
+import json
+from time import sleep
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
@@ -7,25 +8,29 @@ from selenium.webdriver.common.by import By
 from constants import MAIN_URL_MATH, QUESTION
 
 
-if __name__ == '__main__':
+def main():
     response = requests.get(MAIN_URL_MATH + QUESTION)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, features='lxml')
     reshenie = soup.find('div', class_='TemplateSelector_solutionOptions__yKoQF TemplateSelector_multi__1lZyB')
     result = reshenie.find_all('div', class_='TemplateSelector_name__6oev8')
-    result_1 = []
+    result_names = []
     for ans in result:
-        result_1.append(ans.text)
-    result_2 = []
+        result_names.append(ans.text)
+    result_answers = []
     driver = webdriver.Chrome()
     driver.get(MAIN_URL_MATH + QUESTION)
     driver.find_element(By.CLASS_NAME, 'ms-Button.ms-Button--primary.CardButton_cardButton__KYZhg.root-164').click()
-    for answer in result_1:
+    for answer in result_names:
         driver.find_element(By.XPATH, f'//button[normalize-space()="{answer}"]').click()
         result = driver.find_element(By.CLASS_NAME, 'Steps_solutionSteps__a7lQq')
-        result_2.append(result.text)
+        result_answers.append(result.text)
+    answers = dict(zip(result_names, result_answers))
+    return answers
 
-    # result_2 = soup.find_all('div', attrs={'class':'Steps_solutionSteps__a7lQq'})
-    dict_res = {}
-    answer_res = dict(zip(result_1, result_2))
-    print(answer_res)
+
+if __name__ == '__main__':
+    answers = main()
+    FILE_JSON = 'answers.json'
+    with open(FILE_JSON, 'w', encoding='utf-8') as outfile:
+        json.dump(answers, outfile, ensure_ascii=False, indent=4) 
