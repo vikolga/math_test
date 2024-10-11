99e1324fda762d9fe4ea53cb9c071fcb407e0359
vikolga <ov.reatch@gmail.com>
2024-10-11 11:01:48 +0300

    Обновить парсинг и запись в xlsx файл собранных данных

--- ex.py
+++ ex.py
@@ -1,4 +1,5 @@
 
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -11,7 +12,7 @@ import re
 import pandas as pd
 
 
-QUESTIONS_FILE = 'questions.csv'
+QUESTIONS_FILE = 'questions copy.csv'
 bad_escape_re = re.compile(r'\\(?=[^\n\\\'"abfnrtv0-7xNuU])')
 FILE_JSON = 'answers.xls'
 result_answer = {}
@@ -23,17 +24,18 @@ for i in range(1, len(questions)):
     question = questions[i].strip('\n').split('"')
     id = question[0].strip(',')
     zadacha = question[1]
-    print(zadacha)
     if ':' in zadacha:
-        zadacha = re.split(r'\w: ', zadacha)[1]
-        zadacha = bad_escape_re.sub('', zadacha).strip()
-    # if zadacha[:1].isdigit() or zadacha[:1] == '(':
+        z = re.split(r'\w: ', zadacha)
+        if len(z) > 1:
+            zadacha = re.split(r'\w: ', zadacha)[1]
+            zadacha = bad_escape_re.sub('', zadacha).strip()
     result[id] = zadacha
-
 
 option = webdriver.ChromeOptions()
 browser = webdriver.Chrome(options=option)
 for key, value in result.items():
+    if '%' in value or value[0].isalpha():
+        continue
     browser.get(f'https://mathsolver.microsoft.com/ru/solve-problem/{value}')
     html = browser.page_source
 
@@ -41,12 +43,16 @@ for key, value in result.items():
 
 # находим скрипты
     element = soup.find('script', id='__NEXT_DATA__')
+    mess = soup.find('div', class_='MathErrorMessage_message__H_NQf')
+    if mess == 'Мы не можем решить такой тип уравнения, или, возможно, в нем содержится ошибка':
+        continue
     js_el = json.loads(element.text)
     if len(js_el['props']['pageProps']['response']['mathSolverResult']['actions']) == 0:
         continue
     else:
         js_equa = js_el['props']['pageProps']['response']['mathSolverResult']['actions'][0]['templateSteps']
-        result_answer.setdefault('id', []).append(id)
-        result_answer.setdefault('solve', []).append(js_equa)
+        if js_equa != []:
+            result_answer.setdefault('id', []).append(key)
+            result_answer.setdefault('solve', []).append(js_equa)
 df = pd.DataFrame(result_answer)
 df.to_excel('./answers.xlsx')
